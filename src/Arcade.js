933d7d9b4544a79515e3aa34202cb850d2d49d36
Renee Cheng Hoh <rch294@cornell.edu>
2020-08-08 22:52:05 -0700

    update package

--- src/Arcade.js
+++ src/Arcade.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import './App.css';
 import Article from "./Article";
 
@@ -23,7 +22,6 @@ function Arcade() {
             team="Connie Liu, product designer"
             role="Product design, prototyping" 
             tools="Adobe XD, Figma" />
-
 </main>
 
 <footer>
